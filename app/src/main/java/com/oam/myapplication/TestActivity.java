6333d9ff3688d10d3a73cf6d5322833a83102096
omprakash <mrl@321#>
2019-09-24 12:03:34 +0530

    new change

--- app/src/main/java/com/oam/myapplication/TestActivity.java
+++ app/src/main/java/com/oam/myapplication/TestActivity.java
@@ -10,4 +10,8 @@ public class TestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public static String printMessage(String message){
+        return message +"v1.0";
+    }
 }
